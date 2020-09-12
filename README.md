c3b3adbf5691b1757ce1a0ca5e4e7369728899f3
Kevin Webb <kevinlwebb03@gmail.com>
2020-09-12 12:28:28 -0700

    simple web app using HERE

---
+++ README.md
@@ -0,0 +1,12 @@
+# HERE Flask Map
+
+## Instructions (Skip steps 2 and 3 if .pkl file and .db database exists):
+1. Run the following command to install necessary libraries.
+    `pip3 install -r requirements.txt`
+
+2. Create `secret.py` and place in top level directory of the project
+
+3. Run the following command in the app's directory to run the web app.
+    `python3 map.py`
+
+4. Go to http://127.0.0.1:5000
---
+++ map.py
@@ -0,0 +1,46 @@
+
+import requests
+from flask import Flask,render_template
+from secret import api_key
+
+URL = "https://discover.search.hereapi.com/v1/discover"
+latitude = 32.715736
+longitude = -117.161087
+query = 'hospital'
+limit = 5
+
+PARAMS = {
+            'apikey':api_key,
+            'q':query,
+            'limit': limit,
+            'at':'{},{}'.format(latitude,longitude)
+         } 
+
+# sending get request and saving the response as response object 
+r = requests.get(url = URL, params = PARAMS) 
+data = r.json()
+
+hospitals = []
+
+for i in range(5):
+    hospitals.append({
+        'title': data['items'][i]['title'],
+        'address': data['items'][i]['address']['label'],
+        'latitude': data['items'][i]['position']['lat'],
+        'longitude': data['items'][i]['position']['lng'],
+    })
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def map_func():
+	return render_template('map.html',
+                            latitude = latitude,
+                            longitude = longitude,
+                            apikey=api_key,
+                            hospitals = hospitals
+                            )
+
+if __name__ == '__main__':
+	app.run(debug = False)
