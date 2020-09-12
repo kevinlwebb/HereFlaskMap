0db7443a9ea44f0d10cea0cc786c9dae5a320261
Kevin Webb <kevinlwebb03@gmail.com>
2020-09-12 14:44:21 -0700

    changed from API search for hospitals to csv with food pantry locations, added navbar

--- README.md
+++ README.md
@@ -2,12 +2,20 @@
 
 ## Instructions (Skip steps 2 and 3 if .pkl file and .db database exists):
 1. Run the following command to install necessary libraries.
-    `pip3 install -r requirements.txt`
+```
+pip3 install -r requirements.txt
+```
 
 2. Set Environment Variable `HEREAPIKEY` with key. Below is an example for Mac Terminal:
-    `export HEREAPIKEY=givenkeyfromwebsite`
+```
+export HEREAPIKEY=givenkeyfromwebsite
+```
 
-3. Run the following command in the app's directory to run the web app.
-    `python3 map.py`
+3. Place HERE JavaScript key in file `map.html` on line `'apikey': 'placekeyhere'` .
 
-4. Go to http://127.0.0.1:5000
+4. Run the following command in the app's directory to run the web app.
+```
+python3 map.py
+```
+
+5. Go to http://127.0.0.1:5000
--- map.py
+++ map.py
@@ -2,35 +2,94 @@
 import requests
 import os
 from flask import Flask,render_template
+import csv
 
-URL = "https://discover.search.hereapi.com/v1/discover"
 latitude = 32.715736
 longitude = -117.161087
-query = 'hospital'
-limit = 5
 api_key = os.environ.get('HEREAPIKEY')
 
-PARAMS = {
-            'apikey': api_key,
-            'q':query,
-            'limit': limit,
-            'at':'{},{}'.format(latitude,longitude)
-         } 
 
-# sending get request and saving the response as response object 
-r = requests.get(url = URL, params = PARAMS) 
-data = r.json()
+def addr_2_coor(address):
+    URL = "https://geocode.search.hereapi.com/v1/geocode"
+    latitude = 32.715736
+    longitude = -117.161087
+    query = address
+    limit = 5
+    api_key = os.environ.get('HEREAPIKEY')
 
-hospitals = []
+    PARAMS = {
+                'apikey': api_key,
+                'q':query,
+                'limit': limit,
+            } 
 
-for i in range(5):
-    hospitals.append({
-        'title': data['items'][i]['title'],
-        'address': data['items'][i]['address']['label'],
-        'latitude': data['items'][i]['position']['lat'],
-        'longitude': data['items'][i]['position']['lng'],
-    })
+    # sending get request and saving the response as response object 
+    r = requests.get(url = URL, params = PARAMS) 
+    data = r.json()
+    return data['items'][0]['position']
 
+def csv_2_data(path):
+    reader = csv.DictReader(open(path, 'rt'))
+
+    dict_list = []
+
+    for line in reader:
+        dict_list.append(line)
+
+    places = []
+
+    for i in range(5):
+        coor = addr_2_coor(dict_list[i]['Address'] + ", San Diego, California")
+        places.append({
+            'title': dict_list[i]['Resource Parent'],
+            'address': dict_list[i]['Address'] + ", San Diego, California",
+            'latitude': coor['lat'],
+            'longitude': coor['lng'],
+        })
+
+    return places
+
+
+def discover(search):
+    '''
+    Utilizes HERE's discover API to search a word or phrase for Point of Interests
+    '''
+
+    URL = "https://discover.search.hereapi.com/v1/discover"
+    latitude = 32.715736
+    longitude = -117.161087
+    query = search
+    limit = 5
+    api_key = os.environ.get('HEREAPIKEY')
+
+    PARAMS = {
+                'apikey': api_key,
+                'q':query,
+                'limit': limit,
+                'at':'{},{}'.format(latitude,longitude)
+            } 
+
+    # sending get request and saving the response as response object 
+    r = requests.get(url = URL, params = PARAMS) 
+    data = r.json()
+
+    places = []
+
+    for i in range(5):
+        places.append({
+            'title': data['items'][i]['title'],
+            'address': data['items'][i]['address']['label'],
+            'latitude': data['items'][i]['position']['lat'],
+            'longitude': data['items'][i]['position']['lng'],
+        })
+
+    return places
+
+# Utilize this for dynamic information
+#places = discover("food pantry")
+
+#Utilize this for information from a csv
+places = csv_2_data('data/sample-211-food-banks.csv')
 
 app = Flask(__name__)
 
@@ -40,7 +99,7 @@ def map_func():
                             latitude = latitude,
                             longitude = longitude,
                             apikey=api_key,
-                            hospitals = hospitals
+                            places = places
                             )
 
 if __name__ == '__main__':
